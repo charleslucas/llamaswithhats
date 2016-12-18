ca52104dcef9ebc1913b83a640b70e785db44e00
U-AMR\cdlucas <cdlucas@CDLUCAS-MOBL2.amr.corp.intel.com>
2016-12-17 18:46:57 -0800

    New llamaswithhats mod - followed tutorial at http://bedrockminer.jimdo.com/modding-tutorials/#basic

---
+++ src/java/com/lucasi/llamaswithhats/Main.java
@@ -0,0 +1,35 @@
+package com.lucasi.llamaswithhats;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+public class Main {
+
+	    public static final String MODID = "llamaswithhats";
+	    public static final String MODNAME = "Llamas with Hats";
+	    public static final String VERSION = "1.0.0";
+
+	    @Instance
+	    public static Main instance = new Main();
+
+
+	    @EventHandler
+	    public void preInit(FMLPreInitializationEvent e) {
+
+	    }
+
+	    @EventHandler
+	    public void init(FMLInitializationEvent e) {
+
+	    }
+
+	    @EventHandler
+	    public void postInit(FMLPostInitializationEvent e) {
+
+	    }
+}
