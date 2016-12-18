8082b78d70027b75ecfcd42fb43d087a7a599f58
U-AMR\cdlucas <cdlucas@CDLUCAS-MOBL2.amr.corp.intel.com>
2016-12-18 11:28:03 -0800

    Added SideProxies and .gitignore, removed bin and logs directories.

--- src/java/com/lucasi/llamaswithhats/Main.java
+++ src/java/com/lucasi/llamaswithhats/Main.java
@@ -3,6 +3,7 @@ package com.lucasi.llamaswithhats;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,6 +14,9 @@ public class Main {
 	    public static final String MODID = "llamaswithhats";
 	    public static final String MODNAME = "Llamas with Hats";
 	    public static final String VERSION = "1.0.0";
+
+	    @SidedProxy(clientSide="com.lucasi.llamaswithhats.ClientProxy", serverSide="com.lucasi.llamaswithhats.ServerProxy")
+	    public static CommonProxy proxy;
 
 	    @Instance
 	    public static Main instance = new Main();
