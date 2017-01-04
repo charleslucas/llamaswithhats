b3341b6db91f73978d930f5e3a8a9f62e2e67d2f
Charles Lucas <charles@lucas.net>
2017-01-03 22:29:00 -0800

    Added wool armor recipes

--- src/java/com/lucasi/llamaswithhats/crafting/LWHCrafting.java
+++ src/java/com/lucasi/llamaswithhats/crafting/LWHCrafting.java
@@ -3,6 +3,7 @@ package com.lucasi.llamaswithhats.crafting;
 import com.lucasi.llamaswithhats.blocks.LWHBlocks;
 import com.lucasi.llamaswithhats.items.LWHItems;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -10,6 +11,12 @@ public final class LWHCrafting {
 
     public static void init() {
 	   GameRegistry.addRecipe(new ItemStack(LWHBlocks.tutorialBlock), new Object[] {"##", "##", '#', LWHItems.tutorialItem});
+
+	   // Wool Armor Recipes
+	   GameRegistry.addRecipe(new ItemStack(LWHItems.woolBoots), new Object[] {"   ", "# #", "# #", '#', Blocks.WOOL});
+	   GameRegistry.addRecipe(new ItemStack(LWHItems.woolLeggings), new Object[] {"###", "# #", "# #", '#', Blocks.WOOL});
+	   GameRegistry.addRecipe(new ItemStack(LWHItems.woolChestplate), new Object[] {"# #", "###", "###", '#', Blocks.WOOL});
+	   GameRegistry.addRecipe(new ItemStack(LWHItems.woolHelmet), new Object[] {"###", "# #", '#', Blocks.WOOL});
 	}
     
 }
